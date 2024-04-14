08a82633f8ab6e380a49ba1b5211de39d398833d
Alex <mashkal2000@gmail.com>
2024-04-13 17:36:51 -0700

    Add CLI args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser
 import mediapipe as mp
 import cv2
 import numpy as np
@@ -7,37 +8,70 @@ from typing import Tuple, Union
 import math
 import time
 
-# Constants related to image processing
-SMOOTH_NUM_FRAMES = 10  # Number of frames to average over
-# Normalization factor
-SMOOTH_TRIANGLE = SMOOTH_NUM_FRAMES * (SMOOTH_NUM_FRAMES + 1) / 2
-# Triangular weighted moving average - weights n, n-1, ... 2, 1
-SMOOTHING_WEIGHTS = (
-    np.tile(np.arange(SMOOTH_NUM_FRAMES, 0, -1), 4)
-    .reshape(4, SMOOTH_NUM_FRAMES)
-    .transpose()
-)
-# Normalize weights
-SMOOTHING_WEIGHTS_NORMALIZED = SMOOTHING_WEIGHTS / SMOOTH_TRIANGLE
-assert np.array_equal(
-    np.sum(SMOOTHING_WEIGHTS_NORMALIZED, axis=0), [1.0] * 4
-), "Failed to normalize weights"
-LOW_PASS_THRESHOLD = 20  # Changes smaller than this (in pixels) are ignored
-INITIAL_SIZE = 400
 
-# Gaps between the edge of the face and the edge of the window in pixels
-MARGIN_TOP = 75
-MARGIN_BOTTOM = 25
-MARGIN_LEFT = 50
-MARGIN_RIGHT = 50
+def parse_args():
+    # Default values
+    SMOOTH_NUM_FRAMES = 10  # Number of frames to average over
+    LOW_PASS_THRESHOLD = 20  # Changes smaller than this (in pixels) are ignored
+    INITIAL_SIZE = 400
 
-# Constants for drawing debug info on screen
-MARGIN = 10  # pixels
-ROW_SIZE = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-TEXT_COLOR = (255, 0, 0)  # blue - the format is BGR
-DEBUG_LEVEL = 0
+    # Gaps between the edge of the face and the edge of the window in pixels
+    MARGIN_TOP = 75
+    MARGIN_BOTTOM = 25
+    MARGIN_LEFT = 50
+    MARGIN_RIGHT = 50
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--debug",
+        type=int,
+        choices=[0, 1, 2],
+        default=0,
+        help="1: Show bounding boxes for detected face. 2: Also show confidence level and other detected faces",
+    )
+    parser.add_argument(
+        "--low-pass-threshold",
+        type=int,
+        default=LOW_PASS_THRESHOLD,
+        help="Movements smaller than this number (in pixels) are ignored",
+    )
+    parser.add_argument(
+        "--initial-size",
+        type=int,
+        default=INITIAL_SIZE,
+        help="Initial window size (will smoothly scale to actual size)",
+    )
+    parser.add_argument(
+        "--smooth-frames",
+        type=int,
+        default=SMOOTH_NUM_FRAMES,
+        help="Number of frames to smoothly apply changes",
+    )
+    parser.add_argument(
+        "--margin-top",
+        type=int,
+        default=MARGIN_TOP,
+        help="Gap between the top of the face and the edge of the window",
+    )
+    parser.add_argument(
+        "--margin-bottom",
+        type=int,
+        default=MARGIN_BOTTOM,
+        help="Gap between the bottom of the face and the edge of the window",
+    )
+    parser.add_argument(
+        "--margin-left",
+        type=int,
+        default=MARGIN_LEFT,
+        help="Gap between the left of the face and the edge of the window",
+    )
+    parser.add_argument(
+        "--margin-right",
+        type=int,
+        default=MARGIN_RIGHT,
+        help="Gap between the right of the face and the edge of the window",
+    )
+    return parser.parse_args()
 
 
 # Debug utilities
@@ -65,15 +99,37 @@ def _normalized_to_pixel_coordinates(
 
 class State:
     def __init__(self) -> None:
-        self.frame = None
+        self.frame: None | np.ndarray = None
 
 
-def make_visualizer():
+def make_visualizer(args):
     # Mutable state captured inside visualize
     shared_state = State()
     # Start with a fixed initial size
-    trailing_dimensions = np.array([[0, INITIAL_SIZE] * 2] * SMOOTH_NUM_FRAMES)
-    low_pass_last_amounts = np.array([0, INITIAL_SIZE] * 2)
+    trailing_dimensions = np.array([[0, args.initial_size] * 2] * args.smooth_frames)
+    low_pass_last_amounts = np.array([0, args.initial_size] * 2)
+
+    # Constants
+    # Normalization factor
+    SMOOTH_TRIANGLE = args.smooth_frames * (args.smooth_frames + 1) / 2
+    # Triangular weighted moving average - weights n, n-1, ... 2, 1
+    SMOOTHING_WEIGHTS = (
+        np.tile(np.arange(args.smooth_frames, 0, -1), 4)
+        .reshape(4, args.smooth_frames)
+        .transpose()
+    )
+    # Normalize weights
+    SMOOTHING_WEIGHTS_NORMALIZED = SMOOTHING_WEIGHTS / SMOOTH_TRIANGLE
+    assert np.array_equal(
+        np.sum(SMOOTHING_WEIGHTS_NORMALIZED, axis=0), [1.0] * 4
+    ), "Failed to normalize weights"
+
+    # Constants for drawing debug info on screen
+    MARGIN = 10  # pixels
+    ROW_SIZE = 10  # pixels
+    FONT_SIZE = 1
+    FONT_THICKNESS = 1
+    TEXT_COLOR = (255, 0, 0)  # blue - the format is BGR
 
     def visualize(detection_result, image, timestamp) -> None:
         """
@@ -103,15 +159,18 @@ def make_visualizer():
             return
 
         bbox = max_detection.bounding_box
-        start_x = max(bbox.origin_x - MARGIN_LEFT, 0)
-        end_x = min(bbox.origin_x + bbox.width + MARGIN_RIGHT, width)
-        start_y = max(bbox.origin_y - MARGIN_TOP, 0)
-        end_y = min(bbox.origin_y + bbox.height + MARGIN_BOTTOM, height)
+        start_x = max(bbox.origin_x - args.margin_left, 0)
+        end_x = min(bbox.origin_x + bbox.width + args.margin_right, width)
+        start_y = max(bbox.origin_y - args.margin_top, 0)
+        end_y = min(bbox.origin_y + bbox.height + args.margin_bottom, height)
         current_box = np.array([start_y, end_y, start_x, end_x])
 
         # Ignore changes below a threshold
         nonlocal low_pass_last_amounts
-        if np.max(np.abs(current_box - low_pass_last_amounts)) >= LOW_PASS_THRESHOLD:
+        if (
+            np.max(np.abs(current_box - low_pass_last_amounts))
+            >= args.low_pass_threshold
+        ):
             low_pass_last_amounts = current_box
         dimensions = low_pass_last_amounts
 
@@ -129,15 +188,15 @@ def make_visualizer():
             dimensions[0] : dimensions[1], dimensions[2] : dimensions[3]
         ]
 
-        if DEBUG_LEVEL == 0:
+        if args.debug == 0:
             return
-        elif DEBUG_LEVEL == 1:
+        elif args.debug == 1:
             # Draw bounding_box
             start_point = bbox.origin_x, bbox.origin_y
             end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
             cv2.rectangle(opencv_image, start_point, end_point, TEXT_COLOR, 3)
 
-        elif DEBUG_LEVEL == 2:
+        elif args.debug == 2:
             height, width, _ = opencv_image.shape
             for detection in detection_result.detections:
                 # Draw bounding_box
@@ -178,6 +237,7 @@ def make_visualizer():
 
 
 def main():
+    args = parse_args()
     print("Press 'q' to exit")
     BaseOptions = mp.tasks.BaseOptions
     FaceDetector = mp.tasks.vision.FaceDetector
@@ -185,7 +245,7 @@ def main():
     VisionRunningMode = mp.tasks.vision.RunningMode
 
     # Initialize the visuazlier with the captured constants
-    visualizer, state = make_visualizer()
+    visualizer, state = make_visualizer(args)
 
     # Initialize the MediaPipe Face Detector in live stream mode
     options = FaceDetectorOptions(
